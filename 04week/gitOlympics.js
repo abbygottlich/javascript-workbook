e0c70d33d71ea2b7e8d54b5c9314db37b37e6321
Abby Gottlich <abby.gottlich@gmail.com>
2018-10-25 19:06:07 -0500

    git olympics names changed

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const printListOfOlympians = (array) => {
+    array.forEach(function(element){
+        console.log(element);
+    });
+}
+
+printListOfOlympians (['joe', 'sarah', 'sharon', 'jim', 'tim'])
