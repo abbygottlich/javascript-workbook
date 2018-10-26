e9421630e5c49a56a17cafa7da3f4780e260b390
Abby Gottlich <abby.gottlich@gmail.com>
2018-10-25 19:07:11 -0500

    git olympics spaced out and cleaned up

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,9 +1,12 @@
 "use strict"
 
+
+
 const printListOfOlympians = (array) => {
     array.forEach(function(element){
         console.log(element);
     });
 }
+
 
 printListOfOlympians (['joe', 'sarah', 'sharon', 'jim', 'tim'])
