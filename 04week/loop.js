6799d089cb954df09067825b6e0da87a50db8a81
Abby Gottlich <abby.gottlich@gmail.com>
2018-11-03 11:27:15 -0500

    for loop carsInReverse

---
+++ 04week/loop.js
@@ -0,0 +1,8 @@
+// Use a for loop to console.log each item in the array carsInReverse.
+
+const carsInReverse = ["honda", "nissan", "toyota", "volkswagon", "chevy"];
+
+// Starting at 0, loop through every item in the array up to the length of the array. Then print each item(i) in that array.
+for (let i = 0; i <= carsInReverse.length; i++) {
+  console.log(carsInReverse[i]);
+}
