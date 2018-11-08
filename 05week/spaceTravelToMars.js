216b7b3fb2210d14cda635a51e6831fe92d18516
Abby Gottlich <abby.gottlich@gmail.com>
2018-11-08 06:42:51 -0600

    'can enter a ship' function passed

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -10,6 +10,46 @@ let jobTypes = {
 };
 
 // Your code here
+// class #1
+// I created a class called CrewMember and gave it the attributes, 'name', 'job', and 'specialSkill'
+// I also gave it the 'ship' attribute and set it to null, so we can push whichever ship we want into the class
+
+class CrewMember {
+  constructor(name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = null;
+  }
+  enterShip(shipName) {
+    this.ship = shipName;
+    shipName.crew.push(this);
+  }
+}
+
+// class #2
+// I created a new class called 'Ship' and gave it the attributes, 'name', 'type', and 'ability'
+// I also gave it the 'crew' attribute and set it to an empty array, so we can push whichever CrewMember we want into the array
+
+class Ship {
+  constructor(name, type, ability, crew) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+    this.missionStatement = "";
+  }
+}
+
+// I created a new instance of a CrewMember and passed in the same values listed in the CrewMember class
+// I did not include 'ship' because that is the value we will be assigning later
+
+const crewMember1 = new CrewMember("Rick Martinez", "pilot", "chemistry");
+
+// I created a new instance of a Ship and passed in the same values listed in the Ship class
+// I did not include 'crew' because that is the value we will be assigning later
+
+const mav = new Ship("Mars Accent Vehicle", "MAV", "Ascend into low orbit");
 
 //tests
 if (typeof describe === "function") {
@@ -22,24 +62,6 @@ if (typeof describe === "function") {
       assert.equal(crewMember1.ship, null);
     });
 
-    // class #1
-    // I created a class called CrewMember and gave it the attributes, 'name', 'job', and 'specialSkill'
-    // I also gave it the 'ship' attribute and set it to null, so we can push whichever ship we want into the class
-
-    class CrewMember {
-      constructor(name, job, specialSkill, ship) {
-        this.name = name;
-        this.job = job;
-        this.specialSkill = specialSkill;
-        this.ship = null;
-      }
-    }
-
-    // I created a new instance of a CrewMember and passed in the same values listed in the CrewMember class
-    // I did not include 'ship' because that is the value we will be assigning later
-
-    const crewMember1 = new CrewMember("Rick Martinez", "pilot", "chemistry");
-
     it("can enter a ship", function() {
       let mav = new Ship("Mars Ascent Vehicle", "MAV", "Ascend into low orbit");
       let crewMember1 = new CrewMember("Rick Martinez", "pilot", "chemistry");
@@ -50,11 +72,6 @@ if (typeof describe === "function") {
     });
   });
 
-  // enterShip=(crew)=>{
-  //   this.crew = crew
-  //   crew.ship.push(this.name);
-  // }
-
   describe("Ship", function() {
     it("should have a name, a type, an ability and an empty crew upon instantiation", function() {
       let mav = new Ship("Mars Ascent Vehicle", "MAV", "Ascend into low orbit");
@@ -64,40 +81,30 @@ if (typeof describe === "function") {
       assert.equal(mav.crew.length, 0);
     });
 
-    // class #2
-    // I created a new class called 'Ship' and gave it the attributes, 'name', 'type', and 'ability'
-    // I also gave it the 'crew' attribute and set it to an empty array, so we can push whichever CrewMember we want into the array
-
-    class Ship {
-      constructor(name, type, ability, crew) {
-        this.name = name;
-        this.type = type;
-        this.ability = ability;
-        this.crew = [];
-      }
-    }
-
-    // I created a new instance of a Ship and passed in the same values listed in the Ship class
-    // I did not include 'crew' because that is the value we will be assigning later
-
-    const mav = new Ship("Mars Accent Vehicle", "MAV", "Ascend into low orbit");
-
     it("can return a mission statement correctly", function() {
+      //mav is a new instance of a ship (name, type, ability)
       let mav = new Ship("Mars Ascent Vehicle", "MAV", "Ascend into low orbit");
+      //crewMember1 is a new instance of a crewMember (name, job, specialSkill)
       let crewMember1 = new CrewMember("Rick Martinez", "pilot", "chemistry");
+      //hermes is a new instance of a ship (name, type, ability)
       let hermes = new Ship(
         "Hermes",
         "Main Ship",
         "Interplanetary Space Travel"
       );
+      //crewMember2 is a new instance of a crewMember (name, job, specialSkill)
       let crewMember2 = new CrewMember(
         "Commander Lewis",
         "commander",
         "geology"
       );
+      //possible default statement
       assert.equal(mav.missionStatement(), "Can't perform a mission yet.");
       assert.equal(hermes.missionStatement(), "Can't perform a mission yet.");
 
+      //if ship is empty, "can't perform mission yet" is the mission statement
+      //once crewMember enters ship, ship's ability becomes the missionstatement
+      //write a conditional
       crewMember1.enterShip(mav);
       assert.equal(mav.missionStatement(), "Ascend into low orbit");
 
