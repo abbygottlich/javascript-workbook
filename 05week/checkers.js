6b2fa6f25c319dce945d9621b864c74e44479d17
Abby Gottlich <abby.gottlich@gmail.com>
2018-11-10 14:44:17 -0600

    whiteboarding

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -1,12 +1,35 @@
-'use strict';
+// write a for loop for each array to set up the board (for each even space, place a piece)
+// each color can only move in one direction (use greater than or less than)
+// you can't jump your own player
+// put checkers on board
+// 12 white checkers, 12 red checkers
+// switch between two players
+// change space to null after piece has been jumped
+// check for win (all of one type of checker is gone/has been jumped/null)
+// valid move function (can't move to a full spot or white spot)
+// reset board function
 
-const assert = require('assert');
-const readline = require('readline');
+// take in a input
+// split the input
+
+//Write a setGame function to place the pieces on the board (for loop)
+//Write a validMoveX function that only allows X pieces to move down, and into null spots (split the string/use greater than or less than)
+//Write a validMoveO function that only allows O pieces to move up, and into null spots (split the string/use greater than or less than)
+//Write a movePiece function that allows pieces to move from one spot to another
+//Write a jumpOpponent function that allows one piece to jump over another, ommitting the piece that was jumped over
+//Write a switchPlayer function that alternates between Xs and Os
+//Write a XIsKing function that allows the X pieces to move diagonally forward or backwards after one has reached the other side of the board
+//Write a OIsKing function that allows the O pieces to move diagonally forward or backwards after one has reached the other side of the board
+//Write a checkForWin function that says "winner" when one player has captured all of the other players pieces
+
+"use strict";
+
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
 
 function Checker() {
   // Your code here
@@ -14,7 +37,7 @@ function Checker() {
 
 class Board {
   constructor() {
-    this.grid = []
+    this.grid = [];
   }
   // method that creates an 8x8 array, filled with null values
   createGrid() {
@@ -41,11 +64,11 @@ class Board {
           rowOfCheckers.push(this.grid[row][column].symbol);
         } else {
           // just push in a blank space
-          rowOfCheckers.push(' ');
+          rowOfCheckers.push(" ");
         }
       }
       // join the rowOfCheckers array to a string, separated by a space
-      string += rowOfCheckers.join(' ');
+      string += rowOfCheckers.join(" ");
       // add a 'new line'
       string += "\n";
     }
@@ -57,7 +80,7 @@ class Board {
 
 class Game {
   constructor() {
-    this.board = new Board;
+    this.board = new Board();
   }
   start() {
     this.board.createGrid();
@@ -66,8 +89,8 @@ class Game {
 
 function getPrompt() {
   game.board.viewGrid();
-  rl.question('which piece?: ', (whichPiece) => {
-    rl.question('to where?: ', (toWhere) => {
+  rl.question("which piece?: ", whichPiece => {
+    rl.question("to where?: ", toWhere => {
       game.moveChecker(whichPiece, toWhere);
       getPrompt();
     });
@@ -77,30 +100,29 @@ function getPrompt() {
 const game = new Game();
 game.start();
 
-
 // Tests
-if (typeof describe === 'function') {
-  describe('Game', () => {
-    it('should have a board', () => {
-      assert.equal(game.board.constructor.name, 'Board');
+if (typeof describe === "function") {
+  describe("Game", () => {
+    it("should have a board", () => {
+      assert.equal(game.board.constructor.name, "Board");
     });
-    it('board should have 24 checkers', () => {
+    it("board should have 24 checkers", () => {
       assert.equal(game.board.checkers.length, 24);
     });
   });
 
-  describe('Game.moveChecker()', () => {
-    it('should move a checker', () => {
+  describe("Game.moveChecker()", () => {
+    it("should move a checker", () => {
       assert(!game.board.grid[4][1]);
-      game.moveChecker('50', '41');
+      game.moveChecker("50", "41"); //piece starts at 5,0 and jumps to 4,1
       assert(game.board.grid[4][1]);
-      game.moveChecker('21', '30');
+      game.moveChecker("21", "30");
       assert(game.board.grid[3][0]);
-      game.moveChecker('52', '43');
+      game.moveChecker("52", "43");
       assert(game.board.grid[4][3]);
     });
-    it('should be able to jump over and kill another checker', () => {
-      game.moveChecker('30', '52');
+    it("should be able to jump over and kill another checker", () => {
+      game.moveChecker("30", "52");
       assert(game.board.grid[5][2]);
       assert(!game.board.grid[4][1]);
       assert.equal(game.board.checkers.length, 23);
