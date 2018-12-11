9d74c79e574086ab172a9c8dc8cd3ee27c5ce89d
Abby Gottlich <abby.gottlich@gmail.com>
2018-12-10 23:21:30 -0600

    button created and onClick function working

---
+++ 08week/Portfolio/portfolio/src/components/ProjectsComponent.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "../App.css";
+import Button from "@material-ui/core/Button";
+
+class ProjectsComponent extends Component {
+  state = {
+    displayText: "Hello, Welcome to My Portfolio.",
+    buttonText: "View My Projects"
+  };
+
+  handleClick = () => {
+    this.setState({
+      displayText: "My Projects",
+      buttonText: "Home"
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <header>{this.state.displayText}</header>
+        <Button onClick={this.handleClick}>{this.state.buttonText}</Button>
+      </div>
+    );
+  }
+}
+
+export default ProjectsComponent;
